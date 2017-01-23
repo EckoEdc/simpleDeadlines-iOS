d654a22db43d66c84806b154fc2900956871bcdf
Edric MILARET <edric@mbp-de-edric.local>
2017-01-23 13:49:23 -0500

    - add task details view - add task cell class

---
+++ Simple Deadlines/TaskDetailsViewController.swift
@@ -0,0 +1,53 @@
+//
+//  TaskDetailsViewController.swift
+//  Simple Deadlines
+//
+//  Created by Edric MILARET on 17-01-12.
+//  Copyright © 2017 Edric MILARET. All rights reserved.
+//
+
+import UIKit
+import LibSimpleDeadlines
+
+class TaskDetailsViewController: UIViewController {
+
+    @IBOutlet weak var titleTextField: UITextField!
+    @IBOutlet weak var categoryTextField: UITextField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    var task: Task?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        if let t = task {
+            titleTextField.text = t.title
+            categoryTextField.text = t.category?.name
+            datePicker.setDate(Date(timeIntervalSince1970: t.date!.timeIntervalSince1970), animated: false)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func viewWillDisappear(_ animated: Bool) {
+        super.viewWillDisappear(animated)
+        if task != nil, !titleTextField.text!.isEmpty {
+            task!.date = datePicker.date as NSDate
+            TasksService.sharedInstance.save()
+        } else if let title = titleTextField.text, !title.isEmpty {
+            let newTask = TasksService.sharedInstance.getNewTask()
+            newTask.title = titleTextField.text
+            newTask.date = datePicker.date as NSDate
+            TasksService.sharedInstance.save()
+        }
+    }
+    
+    @IBAction func titleChanged(_ sender: Any) {
+        if let t = task {
+            t.title = titleTextField.text
+            t.date = datePicker.date as NSDate
+        }
+    }
+}
